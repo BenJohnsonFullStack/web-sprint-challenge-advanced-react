e94433b4c8270fb779d52e315981c8ccdc2fc902
Ben Johnson <monk.sound.design@gmail.com>
2023-06-29 02:34:20 -0400

    nextIndex

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-import { useState } from 'react'
 
 // Suggested initial states
 const initialMessage = ''
@@ -8,36 +7,89 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
+const URL = "http://localhost:9000/api/result";
+
+const gridArr = [0,1,2,3,4,5,6,7,8];
 
 export default class AppClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: initialMessage,
+      email: initialEmail,
+      index: initialIndex,
+      steps: initialSteps,
+    }
+  }
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
-
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  getXY = (b, x, y) => {
+    if(b === 0) {
+      x = 1;
+      y = 1;
+    } else if(b === 1) {
+      x = 2;
+      y = 1;
+    } else if(b === 2) {
+      x = 3;
+      y = 1;
+    } else if(b === 3) {
+      x = 1;
+      y = 2;
+    } else if(b === 4) {
+      x = 2;
+      y = 2;
+    } else if(b === 5) {
+      x = 3;
+      y = 2;
+    } else if(b === 6) {
+      x = 1;
+      y = 3;
+    } else if(b === 7) {
+      x = 2;
+      y = 3;
+    } else if(b === 8) {
+      x = 3;
+      y = 3;
+    }
+    return `(${x}, ${y})`;
   }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
+    this.setState(this.state.initialMessage);
+    this.setState(this.state.initialIndex);
+    this.setState(this.state.initialEmail);
+    this.setState(this.state.initialSteps);
   }
 
   getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+    if(this.state.index % 3 === 0 && direction === "left") {
+      this.setState({ ...this.state, message: `You can't go ${direction}` });
+      return this.state.index;
+    } else if(this.state.index >= 0 && this.state.index <= 2 && direction === "up") {
+      this.setState({ ...this.state, message: `You can't go ${direction}` });
+      return this.state.index;
+    } else if(this.state.index >= 6 && this.state.index <= 8 && direction === "down") {
+      this.setState({ ...this.state, message: `You can't go ${direction}` });
+      return this.state.index;
+    } else if((this.state.index === 2 || this.state.index === 5 || this.state.index === 8) && direction === "right") {
+      this.setState({ ...this.state, message: `You can't go ${direction}` });
+      return this.state.index;
+    } else if(direction === "left") {
+      this.setState({ ...this.state, index: this.state.index - 1 });
+      this.setState({ ...this.state, steps: this.state.steps + 1 });
+    } else if(direction === "up") {
+      this.setState({ ...this.state, message: this.state.index - 3 });
+      this.setState({ ...this.state, steps: this.state.steps + 1 });
+    } else if(direction === "down") {
+      this.setState({ ...this.state, index: this.state.index + 3 });
+      this.setState({ ...this.state, steps: this.state.steps + 1 });
+    } else if(direction === "right") {
+      this.setState({ ...this.state, index: this.state.index + 1 });
+      this.setState({ ...this.state, steps: this.state.steps + 1 });
+    }
+    return this.state.index;
   }
 
   move = (evt) => {
@@ -64,10 +116,10 @@ export default class AppClass extends React.Component {
       console.error(err);
     })
     .finally(() => {
-      this.setState(this.initialState.initialMessage);
-      this.setState(this.initialState.initialIndex);
-      this.setState(this.initialState.initialEmail);
-      this.setState(this.initialState.initialSteps);
+      this.setState({ message: this.state.initialMessage });
+      this.setState({ index: this.state.initialIndex });
+      this.setState({ email: this.state.initialEmail });
+      this.setState({ steps: this.state.initialSteps });
     })
   }
 
